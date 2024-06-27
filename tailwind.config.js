7e430125820ca1226a5c9f8be27769375930110e
parajuligovinda541 <parajuligovinda541@gmail.com>
2024-06-27 22:22:39 +0545

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+const withMT = require("@material-tailwind/html/utils/withMT");
+
+module.exports = withMT({
+  content: ["index.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
       },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
+) ;
